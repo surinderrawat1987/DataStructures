21f4e5471a39497a9ac69783666b54f356b1e1e5
root <root@localhost>
2020-01-15 18:55:03 +0530

    fixes as per test cases

--- RoundArray.c
+++ RoundArray.c
@@ -1,8 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ * Given an unsorted array arr[] of size N, rotate it by D elements (clockwise). 
+
+    Input:
+    The first line of the input contains T denoting the number of testcases. First line of each test case contains two space separated elements, N denoting the size of the array and an integer D denoting the number size of the rotation. Subsequent line will be the N space separated array elements.
+
+    Output:
+    For each testcase, in a new line, output the rotated array.
+
+    Constraints:
+    1 <= T <= 200
+    1 <= N <= 107
+    1 <= D <= N
+    0 <= arr[i] <= 105
+
+    Example:
+    Input:
+    2
+    5 2
+    1 2 3 4 5 
+    10 3
+    2 4 6 8 10 12 14 16 18 20
+
+    Output:
+    3 4 5 1 2
+    8 10 12 14 16 18 20 2 4 6
+
+    Explanation :
+    Testcase 1: 1 2 3 4 5  when rotated by 2 elements, it becomes 3 4 5 1 2.
+
+
+    ** For More Input/Output Examples Use 'Expected Output' option **
+ **/
 
 /* 
  * File:   main.c
@@ -28,7 +56,7 @@ void main() {
             scanf("%d",&a[j]);
         }
         
-        for(j=totalInt-1;j>=rotate;j--){
+        for(j=rotate;j<totalInt;j++){
             printf("%d",a[j]);
             printf("%s"," ");
         }
@@ -37,7 +65,6 @@ void main() {
             printf("%d",a[j]);
             printf("%s"," ");
         }
-        
+    printf("%s","\n");    
     }
 }
-
